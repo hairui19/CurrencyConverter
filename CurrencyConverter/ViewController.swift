4ae2323baeedfe34721e19371945217cedc6d4d6
Hairui <hairui@sorealprop.com>
2018-04-04 09:10:40 +0800

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Hairui on 4/4/18.
+//  Copyright © 2018 Hairui's Organisation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
