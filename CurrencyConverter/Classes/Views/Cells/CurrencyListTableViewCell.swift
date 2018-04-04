6552327303e005202d8819cb35bfaca5f3b7ee9a
Hairui <hairui@sorealprop.com>
2018-04-04 16:00:56 +0800

    Complete testing CurrencyListViewModel

---
+++ CurrencyConverter/Classes/Views/Cells/CurrencyListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyListTableViewCell.swift
+//  CurrencyConverter
+//
+//  Created by Hairui on 4/4/18.
+//  Copyright © 2018 Hairui's Organisation. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
