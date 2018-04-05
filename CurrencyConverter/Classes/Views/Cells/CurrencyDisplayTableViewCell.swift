9e96644a0fecd6c587552eff22cbc9b6ef17c1a4
Hairui <hairui@sorealprop.com>
2018-04-05 17:04:11 +0800

    Create CurrencyDisplayTableViewCell

---
+++ CurrencyConverter/Classes/Views/Cells/CurrencyDisplayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyDisplayTableViewCell.swift
+//  CurrencyConverter
+//
+//  Created by Hairui on 5/4/18.
+//  Copyright © 2018 Hairui's Organisation. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyDisplayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
